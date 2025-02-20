4703e89ac1e7c32554b4092d433deee89c9b26be
Emanuele Macera <emanuele.macera@protonmail.com>
2025-02-20 14:03:23 +0100

    Add new "Spa" section with image and service details for enhanced user experience

---
+++ resources/views/blocks/homepage_dettagli.blade.php
@@ -0,0 +1,28 @@
+<div class="container">
+    <div class="row align-items-center">
+        <div class="col-md-6">
+            <!-- Placeholder per l'immagine della spa -->
+            <img src="url('{{ asset('assets/custom/homepage/layer_2.WebP') }}')" alt="Spa e zona benessere" class="img-fluid">
+        </div>
+        <div class="col-md-6">
+            <p>Lo spazio benessere attiguo alla sala ludica e e direttamente accessibile dalle zone comuni del complesso, offre il vostro desiderato percorso salute & relax con:</p>
+            <ul>
+                <li>Sauna</li>
+                <li>Hamman</li>
+                <li>Vasca idromassaggio con zona relax privata</li>
+                <li>Docce emozionali</li>
+                <li>Tisaneria</li>
+                <li>Corner relax</li>
+            </ul>
+            <p>Potrete contare sulla disponibilità di esperti operatori associati al club per consigli generali e prenotazioni di trattamenti personalizzati. Un percorso relax unico e piacevole per un benessere a cinque sensi.</p>
+            <p class="alert alert-warning"><strong>Nota:</strong> Tutti i servizi indicati sono utilizzabili esclusivamente su prenotazione!</p>
+            <div class="contact-info">
+                <h4>Per info e prenotazioni:</h4>
+                <p>
+                    <a href="tel:+393534570629">+39 353 457 0629</a><br>
+                    <a href="tel:+390171926669">+39 0171 926669</a> int. 2
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
